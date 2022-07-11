9dc341ef1ee202dfdf1aad2b228db0145d3032be
Stefane Castoldi <stefanecastoldi@dcc.ufrj.br>
2022-07-10 22:42:35 -0300

    arquivos A1 e P2 prontos

--- A1-analytica.py
+++ A1-analytica.py
@@ -1,80 +1,211 @@
 # A1
 
-# Numpy ----------------------
+# Imports ------------------------------------------------------
 
+import aifc
+from distutils.log import error
 import numpy as np
 import math 
 from numpy import random
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Numpy ---------------------------------------------------------
+
+# Iteração - questão 4
+
+# (a)
+
+a = np.array([5.5, 3.8, 9, 7.5, 10.0, 9.9, 8.5])
+
+a_sum = 0
+a_dp1 = 0
+
+for number in a:
+    a_sum += number
+    #a_sum = number + a_sum
+
+a_avg = a_sum / len(a)
+
+print('a média dos números dessa lista: ', a_avg)
+
+# (b)
+
+for number in a:
+    a_dp1 += (number - a_avg)**2 
+
+a_dp2 = math.sqrt((a_dp1) / len(a))
+
+print('o desvio padrão dos números dessa lista: ',a_dp2)
+
+# Funções - Questão 5
+
+# (a)
+
+print(np.sort(a))
+
+# (b)
+
+print(np.shape(a))
+
+# (c)
+
+print(a.mean())
+
+# (d)
+
+print(a.std())
+print(a.max())
+print(a.min())
+
+# (e)
+
+n = random.randint(11, size=(100))
+
+print(n)
+
+# média
+print(n.mean())
+
+# desvio padrao
+print(n.std())
+
+# valor máximo
+print(n.max())
+
+# valor mínimo
+print(n.min())
 
 
-# # Iteração - questão 4
+# Pandas --------------------------------------------
 
-# #(a)
+# 1.2.2 Parte Prática 
 
-# a = np.array([5.5, 3.8, 9, 7.5, 10.0, 9.9, 8.5])
+# Questão 1
 
-# a_sum = 0
-# a_dp1 = 0
+dados = pd.read_csv('./Desktop/Analytica/Dataset.csv')
+ 
 
-# for number in a:
-#     a_sum += number
-#     #a_sum = number + a_sum
+# Questão 2
 
-# a_avg = a_sum / len(a)
+print(dados)
 
-# print('a média dos números dessa lista: ', a_avg)
+# Questão 3
 
-# #(b)
+# (a)
 
-# for number in a:
-#     a_dp1 += (number - a_avg)**2 
+filtro_ano = dados["ano"] == 1991
+ano_filtrado = dados[filtro_ano] 
 
-# a_dp2 = math.sqrt((a_dp1) / len(a))
-# #a_avg = a_sum / len(a)
+print(ano_filtrado)
 
-# print('o desvio padrão dos números dessa lista: ',a_dp2)
+# (b)
 
-# # Funções - questão 5
+print(ano_filtrado['expectativa_vida'])
 
-# #(a)
+# (c)
 
-# print(np.sort(a))
+print(ano_filtrado['expectativa_vida'].mean())
 
-# #(b)
+# Questão 4
 
-# print(np.shape(a))
+# (a)
 
-# #(c)
+filtro_idh_maior = ano_filtrado["idhm"] > ano_filtrado['expectativa_vida'].mean()
+idh_maior_filtrado = ano_filtrado[filtro_idh_maior]
 
-# print(a.mean())
+print(idh_maior_filtrado)
 
-# #(d)
+# (b)
 
-# print(a.std())
-# print(a.max())
-# print(a.min())
+print(idh_maior_filtrado["sigla_uf"])
 
-# #(e)
+# Questão 5
+
+ano_idhm = ano_filtrado[["ano", "idhm"]]
+
+print(ano_idhm.sort_values(by=['expectativa_vida']))
+
+print(ano_idhm.sort_values(by=['expectativa_vida']).head(5))
+
+# Questão 6
+
+ano_sigla_idhm = ano_filtrado[["ano","sigla_uf", "idhm"]]
+
+idh = ano_filtrado["idhm"] 
+
+idh_min = ano_sigla_idhm[ano_sigla_idhm["idhm"] == idh.min()] 
+
+print(idh_min) 
+
+idh_max = ano_sigla_idhm[ano_sigla_idhm["idhm"] == idh.max()] 
+
+print(idh_max)
+
+idh_idxmin = idh.idxmin() 
+
+print(ano_sigla_idhm.iloc[idh_idxmin])
+
+idh_idxmax = idh.idxmax() 
+
+print(ano_sigla_idhm.iloc[idh_idxmax])
+
+# Matplotlib ------------------------------------------------------------
+
+# Questão 1
+
+estados = (dados.loc[:, 'sigla_uf']).unique()
+
+qnt_pop = ano_filtrado.loc[:, 'populacao_urbana'].values
+
+plt.bar(estados,qnt_pop)
+plt.show()
+
+# Questão 2
+
+ano_2000 = dados["ano"] == 2000
+ano_2000_filtrado = dados[ano_2000]
+
+ano_2010 = dados["ano"] == 2010
+ano_2010_filtrado = dados[ano_2010]
+
+plt.hist(ano_filtrado["idhm"], bins = 10)
+plt.show()
 
 
-# n = random.randint(11, size=(100))
-
-# print(n)
-
-# # média
-# print(n.mean())
-
-# # desvio padrao
-# print(n.std())
-
-# # valor máximo
-# print(n.max())
-
-# # valor mínimo
-# print(n.min())
+plt.hist(ano_2000_filtrado["idhm"], bins = 10)
+plt.show()
 
 
-# Pandas ----------------------
+plt.hist(ano_2010_filtrado["idhm"], bins = 10)
+plt.show()
 
-dados = pd.read_csv('./')
+# Questão 3
+
+exp_vida = np.array([dados["expectativa_vida"]])
+idh = np.array([dados["idhm"]])
+
+plt.scatter(exp_vida, idh)
+plt.show()
+
+"""
+De acordo com os pontos observados no Scatter Plot sobre os dados relativos à expectativa de vida e o IDH, podemos ver que o valor dos dois dados vão crescendo mutualmente, ou seja, vão crecendo em conjunto, portanto, existe uma relação direta entre as duas medidas.
+"""
+
+# Desafio Final ------------------------------------------------------------------
+
+expec_vida_1991 = ano_filtrado["expectativa_vida"]
+
+expec_vida_2010 = ano_2010_filtrado["expectativa_vida"]
+
+dif_expec_vida = expec_vida_2010.values - expec_vida_1991.values
+
+plt.bar(estados,dif_expec_vida)
+plt.show() 
+
+estados_maior_expec = pd.DataFrame({"sigla_uf": estados,"diferenca_expec_vida": dif_expec_vida})
+
+expec_vida_maior10 = estados_maior_expec["diferenca_expec_vida"] >= 10
+expec_maior10_filtrado = estados_maior_expec[expec_vida_maior10]
+
+print(expec_maior10_filtrado["sigla_uf"])
